20bddfa403e4928b58582499cd4ed6f4e3c01e5f
Alejandro Biancucci <32583931+abiancu@users.noreply.github.com>
2024-02-28 07:50:07 -0600

    Adding more style to the movie details page. (#7)
    * Update build actions name.
    
    * add pills for movie home page and ratings - wip
    
    * Cleaning up all build warning.

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -2,8 +2,8 @@ namespace BlazorMovieDB.Utilities
 {
     public static class Constants
     {
-        public const string discoverMovie = "discover/movie";
-        public const string discoverTv = "discover/tv";
-        public const string movieDetails = "movie";
+        public const string DiscoverMovie = "discover/movie";
+        public const string DiscoverTv = "discover/tv";
+        public const string MovieDetails = "movie";
     }
 }
