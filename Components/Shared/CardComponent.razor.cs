ec18a884c860c8184d9999bd1bbf61d548162be6
Alejandro Biancucci <alejandrobiancucci@gmail.com>
2024-03-13 07:59:29 -0500

    Add oninitialized to component.

--- Components/Shared/CardComponent.razor.cs
+++ Components/Shared/CardComponent.razor.cs
@@ -6,4 +6,9 @@ namespace BlazorMovieDB.Components.Shared;
 public partial class CardComponent : ComponentBase
 {
     [Parameter] public Movie? Movie { get; set;  }
+
+    protected override Task OnInitializedAsync()
+    {
+       return Task.CompletedTask;
+    }
 }
