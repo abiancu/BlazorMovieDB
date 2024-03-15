e6fa2e7be5479504a2a8b0e873e57cccf02c3151
Alejandro Biancucci <alejandrobiancucci@gmail.com>
2024-03-14 21:54:07 -0500

    Card component for tv and movies

--- Components/Shared/CardComponent.razor.cs
+++ Components/Shared/CardComponent.razor.cs
@@ -1,3 +1,4 @@
+using BlazorMovieDB.Components.Services;
 using BlazorMovieDB.Models;
 using Microsoft.AspNetCore.Components;
 
@@ -6,9 +7,11 @@ namespace BlazorMovieDB.Components.Shared;
 public partial class CardComponent : ComponentBase
 {
     [Parameter] public Movie? Movie { get; set;  }
+    [Parameter] public Tv? Tv { get; set; }
+    [Parameter] public string Type { get; set; } = "movie";
 
     protected override Task OnInitializedAsync()
     {
-       return Task.CompletedTask;
+        return Task.CompletedTask;
     }
 }
